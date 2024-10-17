2db5b16493afaeebeca17c5a50846e8ec914e876
Sjur <sjursaa@gmail.com>
2024-10-17 19:35:54 +0200

    add constructor for book object

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("Hello World");
+
+const myLibrary = [];
+
+function book(title, author, pages, read) {
+  // ctor
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    // TODO: format string properly
+    return this.title + this.author + this.pages + this.read;
+  };
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
