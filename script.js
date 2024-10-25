2c87061ba37016063eeefe398bd1a687e863309c
Sjur <sjursaa@gmail.com>
2024-10-25 22:25:20 +0200

    create form for submitting book parameters

--- script.js
+++ script.js
@@ -47,5 +47,37 @@ addBookButton.onclick = () => addBookToLibrary();
 
 console.log(myLibrary);
 
+const container = document.querySelector("#container");
+const form = document.createElement("FORM");
+form.style.color = "blue";
+form.setAttribute("id", "form");
+// form.textContent = "hello world";
+container.appendChild(form);
+
+const inputTitle = document.createElement("INPUT");
+inputTitle.setAttribute("type", "text");
+inputTitle.setAttribute("value", "title");
+form.appendChild(inputTitle);
+
+const inputAuthor = document.createElement("INPUT");
+inputAuthor.setAttribute("type", "text");
+inputAuthor.setAttribute("value", "author");
+form.appendChild(inputAuthor);
+
+const inputPages = document.createElement("INPUT");
+inputPages.setAttribute("type", "text");
+inputPages.setAttribute("value", "pages");
+form.appendChild(inputPages);
+
+const inputRead = document.createElement("INPUT");
+inputRead.setAttribute("type", "text");
+inputRead.setAttribute("value", "y/N");
+form.appendChild(inputRead);
+
+const submitButton = document.createElement("BUTTON");
+submitButton.setAttribute("type", "submit");
+submitButton.setAttribute("value", "submit");
+form.appendChild(submitButton);
+
 // TODO: add button to each books display for removing them (DOM)
 // TODO: add button for each books display for changing read status (DOM)
