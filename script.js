cc1fa307975e398a73acf3290d9c2d6c71be43e5
Sjur <sjursaa@gmail.com>
2024-10-30 14:08:39 +0100

    add contents of array to table

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function addBookToLibrary() {
 const theHobbit = new book(
   "The Hobbit",
   "J.R.R. Tolkien",
-  "295 pages",
+  "367 pages",
   "not read yet",
 );
 
@@ -44,6 +44,7 @@ console.log(theHobbit.info());
 
 const addBookButton = document.querySelector("#add-new-book");
 addBookButton.onclick = () => createForm();
+// addBookButton.onclick = () => fillTable();
 
 console.log(myLibrary);
 
@@ -103,44 +104,57 @@ function createForm() {
   };
 }
 
-const container = document.querySelector("#container");
-const table = document.createElement("TABLE");
-table.setAttribute("id", "myTable");
-table.setAttribute("border", "solid");
-// table.style.backgroundColor = "blue";
-container.appendChild(table);
+function fillTable() {
+  const container = document.querySelector("#container");
+  const table = document.createElement("TABLE");
+  table.setAttribute("id", "myTable");
+  table.setAttribute("border", "solid");
+  // table.style.backgroundColor = "blue";
+  container.appendChild(table);
 
-const row = document.createElement("TR");
-row.setAttribute("id", "myTr");
-table.appendChild(row);
+  const row = document.createElement("TR");
+  row.setAttribute("id", "myTr");
+  table.appendChild(row);
 
-const td = document.createElement("TD");
-const textNode = document.createTextNode("Title");
-td.appendChild(textNode);
-row.appendChild(td);
+  const td = document.createElement("TD");
+  const textNode = document.createTextNode("Title");
+  td.appendChild(textNode);
+  row.appendChild(td);
 
-const td2 = document.createElement("TD");
-const textNode2 = document.createTextNode("Author");
-td2.appendChild(textNode2);
-row.appendChild(td2);
+  const td2 = document.createElement("TD");
+  const textNode2 = document.createTextNode("Author");
+  td2.appendChild(textNode2);
+  row.appendChild(td2);
 
-const td3 = document.createElement("TD");
-const textNode3 = document.createTextNode("Pages");
-td3.appendChild(textNode3);
-row.appendChild(td3);
+  const td3 = document.createElement("TD");
+  const textNode3 = document.createTextNode("Pages");
+  td3.appendChild(textNode3);
+  row.appendChild(td3);
 
-const td4 = document.createElement("TD");
-const textNode4 = document.createTextNode("Read");
-td4.appendChild(textNode4);
-row.appendChild(td4);
+  const td4 = document.createElement("TD");
+  const textNode4 = document.createTextNode("Read");
+  td4.appendChild(textNode4);
+  row.appendChild(td4);
 
-const row2 = document.createElement("TR");
-row2.setAttribute("id", "myTr");
-table.appendChild(row2);
+  myLibrary.forEach((element) => {
+    console.log(element);
+    let row2 = document.createElement("TR");
+    row2.setAttribute("id", "myTr");
+    table.appendChild(row2);
 
-myLibrary.forEach((element) => {
-  console.log(element);
-});
+    let td = document.createElement("TD");
+    let textNode = document.createTextNode(element.info());
+    console.log(element.info());
+    td.appendChild(textNode);
+    row2.appendChild(td);
+  });
+}
+
+fillTable();
+
+// const row2 = document.createElement("TR");
+// row2.setAttribute("id", "myTr");
+// table.appendChild(row2);
 
 // TODO: add button to each books display for removing them (DOM)
 // TODO: add button for each books display for changing read status (DOM)
